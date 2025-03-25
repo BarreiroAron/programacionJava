fdfd598ef294ffffbff8d130a1cf614eaabd98f9
aronBarreiro <130598856+BarreiroAron@users.noreply.github.com>
2025-03-25 18:11:10 -0300

    Create ejercicios_aad_Cuatro.java
    xd

---
+++ ejerciciosPrimeraGuia/ejercicios_aad_Cuatro.java
@@ -0,0 +1,17 @@
+package ejerciciosJava;
+
+public class ejercicios_aad_Cuatro {
+	public static void main(String[] args) {
+		/* Se poseen 4 atributos:  
+			 a de tipo short 
+			 b de tipo long 
+			 c de tipo float  
+			 d de tipo String */
+		short a = 1;
+		long b = 200;
+		float c = 32.29f;
+		String d = "palabraRandom";
+		System.out.println("Las variables que puse en el ejercicio son: " + a + " / Para el short");
+		System.out.println(" / el long: " + b + " / el float: " + c + " / y el String: " + d);
+	}
+}

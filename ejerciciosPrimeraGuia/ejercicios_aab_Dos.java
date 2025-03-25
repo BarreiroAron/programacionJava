c69c3760e30bda9e33f9f134a762e1c14b42cfa4
aronBarreiro <130598856+BarreiroAron@users.noreply.github.com>
2025-03-25 17:48:10 -0300

    Create ejercicios_aab_Dos.java
    xd

---
+++ ejerciciosPrimeraGuia/ejercicios_aab_Dos.java
@@ -0,0 +1,9 @@
+package ejerciciosJava;
+
+public class ejercicios_aab_Dos {
+	public static void main(String[] args) {
+		//Indique el valor de x = -1 + (5 /3) % 2 * (5 + 7) * 2 + 1 
+		float x = -1 + (5/3) % 2 * (5+7) * 2+1;
+		System.out.println("el valor de la x es: " + x);
+	}
+}

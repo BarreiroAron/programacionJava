4985a23452b1073f504b9c58e350c39cf50bf423
aronBarreiro <130598856+BarreiroAron@users.noreply.github.com>
2025-04-08 04:43:21 +0200

    Update lollapalooza.java
    xd

--- tpNivelador/lollapalooza.java
+++ tpNivelador/lollapalooza.java
@@ -1,6 +1,6 @@
 package tpNivelador;
 
-//import java.util.Inp1utMismatchException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class lollapalooza {
@@ -10,6 +10,7 @@ public class lollapalooza {
 		final int ID_MIN = 1, ID_MAX = 999;
 		int mueveAlmacenamiento = 0;
 		Scanner s = new Scanner(System.in);
+		boolean salir = true;
 		
 		final boolean MODO_PRUEBA = true;
 		if(MODO_PRUEBA == true) {
@@ -21,34 +22,53 @@ public class lollapalooza {
 		System.out.println("¡Bienvenido a Lollapalooza!");
 		System.out.println("Por favor, eliga una opción para continuar.");
 		System.out.println("-          -          -          -          -");
-		System.out.println("Registrar Artista/Banda (1)");
-		System.out.println("Consultar Artista/Banda (2)");
+		System.out.println("Registrar Artista/Banda (1)"); //hecho
+		System.out.println("Consultar Artista/Banda (2)"); //hecho
 		System.out.println("Modificar datos de Artista/Banda (3)");
 		System.out.println("Eliminar Artista/Banda (4)");
-		System.out.println("Lista de Artistas/Bandas (5)");
+		System.out.println("Lista de Artistas/Bandas (5)"); //hecho
 		System.out.println("Cronograma en el dia (6)");
 		System.out.println("Cronograma por escenario (7)");
-		System.out.println("Buscar Artista/Banda por Genero (8)");
-		System.out.println("Buscar Artista/Banda pro Popularidad (9)");
-		System.out.println("Estadisticas del festival");
+		System.out.println("Buscar Artista/Banda por Genero (8)"); //en progreso
+		System.out.println("Buscar Artista/Banda pro Popularidad (9)"); //hecho
+		System.out.println("Estadisticas del festival (10)");
+		System.out.println("Salir (11)"); //en progreso
 		System.out.println("------------------------------------------------");
 		int opcion = s.nextInt();
-		mueveAlmacenamiento = mostrarMenuYElegirOpcion(opcion, s, artista, mueveAlmacenamiento, ID_MIN, ID_MAX);
-		} while(true);
+		mueveAlmacenamiento = mostrarMenuYElegirOpcion(opcion, s, artista, mueveAlmacenamiento);
+		} while(salir);
 	}
 	
-	public static int mostrarMenuYElegirOpcion(int opcion, Scanner s, String[][] artista, int mueveAlmacenamiento, final int ID_MIN, final int ID_MAX) {
+	public static int mostrarMenuYElegirOpcion(int opcion, Scanner s, String[][] artista, int mueveAlmacenamiento) {
 		switch(opcion) {
 		case 1:
-			mueveAlmacenamiento = ingresarArtista(s, artista, mueveAlmacenamiento, ID_MIN, ID_MAX );
+			mueveAlmacenamiento = ingresarArtista(s, artista, mueveAlmacenamiento);
 			break;
 		case 2:
 			buscarArtista(s, artista, mueveAlmacenamiento, mueveAlmacenamiento);
 			break;
 		case 3:
-			System.out.println("no xd");
+			System.out.println("No hecha");
 			break;
 		case 4:
+			System.out.println("No hecha");
+			break;
+		case 5:
+			listarArtistas(artista, mueveAlmacenamiento, mueveAlmacenamiento);
+			break;
+		case 6:
+			verCronogramaPorDia(artista);
+			break;
+		case 7:
+			break;
+		case 8:
+			break;
+		case 9:
+			buscarArtistasPorPopularidad(s, artista, mueveAlmacenamiento, mueveAlmacenamiento, mueveAlmacenamiento);
+			break;
+		case 10:
+			break;
+		case 11:
 			saliendo();
 			break;
 			default:
@@ -57,11 +77,10 @@ public class lollapalooza {
 		return mueveAlmacenamiento;
 	}
 	
-	public static int ingresarArtista(Scanner s, String[][] artista, int mueveAlmacenamiento, final int ID_MIN, final int ID_MAX) {
+	public static int ingresarArtista(Scanner s, String[][] artista, int mueveAlmacenamiento) {
+		System.out.println("Ingrese el artista/banda");
 		System.out.println("Ingrese una ID (desde 1 hasta 999):");
 		final int ID = s.nextInt();
-		validarID(ID,ID_MIN,ID_MAX);
-		chekearID(ID,artista,mueveAlmacenamiento);
 		s.nextLine();
 		System.out.println("Ingrese el nobmre de la banda o el artista que va a tocar:");
 		final String NOMBRE_BANDA_ARTISTA = s.nextLine();
@@ -100,18 +119,11 @@ public class lollapalooza {
 		//Creo que aca tengo que usar los try catch pero realmente no se como avanzar con esta funcion
 	}
 	
-	public static void validarID(int ID, final int ID_MIN, final int ID_MAX) {
-		if(ID>ID_MAX || ID<ID_MIN) {
+	public void validarID(int ID, final int ID_MIN, final int ID_MAX) {
+		//Creo que aca tengo que usar los try catch pero realmente no se como avanzar con esta funcion
+		if(ID_MIN > ID_MAX) {
 			System.out.println("Error, el valor minimo no puede ser mayor al maximo");
 			System.exit(1);
-		}
-	}
-	
-	public static void chekearID(int ID, String[][] artista, int mueveAlmacenamiento ) {
-		for(int i = 0; i < mueveAlmacenamiento; i++) {
-			if(Integer.parseInt(artista[i][0]) == ID) {
-				System.out.println("Error ya existe");
-			}
 		}
 	}
 	
@@ -129,10 +141,50 @@ public class lollapalooza {
 		}
 	}
 	
-	public static void saliendo() {
-		//Creo que esta era la forma que el profe no queria que usasemos, pero por ahora tenemos esto
+	public static void listarArtistas(String[][] artista, final int ID, int mueveAlmacenamiento) {
+		for(int i = 0; i < mueveAlmacenamiento; i++) {
+				/*for(int j = 0; j < artista[i].length; j++) {
+					System.out.println(" " + artista[i][j]);
+				}*/
+				System.out.println("|Id: " + artista[i][0] + "| Nombre del Artista/Banda: " + artista[i][1] + " |Genero: " + artista[i][2] + " |Dia de presentacion: " + artista[i][3]);
+				System.out.println("|Escenario: " + artista[i][4] + " |Hora de inicio " + artista[i][5] + " |Duracion en minutos: " + artista[i][6] + " |Popularidad de la banda: " + artista[i][7]);
+				System.out.println(" ");
+		}
+	}
+	
+	public static void verCronogramaPorDia(String[][] artsita) {
+		
+	}
+	
+	/*public static void buscarArtistasPorGenero(Scanner s, String[][] artista, final int GENERO_MUSICAL, int generoMomentaneo, int mueveAlmacenamiento) {
+		System.out.println("Por favor, ingrese el genero de su banda: ");
+		System.out.println("Recuerde que: 1 \\\"Rock\\\" - 2  \\\"Pop\\\" - 3  \\\"Electrónica\\\" - 4  \\\"Hip Hop\\\" - 5  \\\"Indie\\\" - 6  \\\"Metal\\\" - 7  \\\"Otros\\");
+		generoMomentaneo = s.nextInt();
+			for(int i = 0; i < mueveAlmacenamiento; i++ ) {
+				if(generoMomentaneo == Integer.parseInt(artista[i][GENERO_MUSICAL])) {
+			System.out.println("|Id: " + artista[i][0] + "| Nombre del Artista/Banda: " + artista[i][1] + " |Genero: " + artista[i][2] + " |Dia de presentacion: " + artista[i][3]);
+			System.out.println("|Escenario: " + artista[i][4] + " |Hora de inicio " + artista[i][5] + " |Duracion en minutos: " + artista[i][6] + " |Popularidad de la banda: " + artista[i][7]);
+			System.out.println(" ");
+			}
+		}
+	}              ESTA FUNCION AUN TIRA ERROR*/
+	
+	public static void buscarArtistasPorPopularidad(Scanner s, String[][] artista, final int POPULARIDAD, int popularidadMomentanea, int mueveAlmacenamiento) {
+		System.out.println("Por favor, ingrese la popularidad que tiene su banda: ");
+		popularidadMomentanea = s.nextInt();
+		for(int i = 0; i < mueveAlmacenamiento; i++) {
+			if(popularidadMomentanea == Integer.parseInt(artista[i][7])) {
+		System.out.println("|Id: " + artista[i][0] + "| Nombre del Artista/Banda: " + artista[i][1] + " |Genero: " + artista[i][2] + " |Dia de presentacion: " + artista[i][3]);
+		System.out.println("|Escenario: " + artista[i][4] + " |Hora de inicio " + artista[i][5] + " |Duracion en minutos: " + artista[i][6] + " |Popularidad de la banda: " + artista[i][7]);
+		System.out.println(" ");
+			}
+		}
+	}
+	
+	public static boolean saliendo() { //tampoco anda bien por ahora
+		boolean salir = false;
 		System.out.println("Saliendo . . .");
-		System.exit(0);
+		return salir;
 	}
 	
     // Método para cargar datos de prueba precargados
